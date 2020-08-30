f2852a68ddb073afd3959f26bc00cd5d39a70511
MoNwastaken <nhcmon@gmail.com>
2020-08-30 12:26:52 +0200

    Added components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FeaturedComponent } from './home/featured/featured.component';
+import { PostsComponent } from './home/posts/posts.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FeaturedComponent,
+    PostsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
