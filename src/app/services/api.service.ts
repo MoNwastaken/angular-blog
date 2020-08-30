a5ac48d5971f43ccd8d4f700313ea51de33a8969
MoNwastaken <nhcmon@gmail.com>
2020-08-30 13:32:35 +0200

    Getting posts works

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  getPostsURL = "https://jsonplaceholder.typicode.com/posts";
+  getCommentsURL = "https://jsonplaceholder.typicode.com/posts/1/comments";
+  addPostsURL = "https://jsonplaceholder.typicode.com/posts";
+  posts: Post[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getPosts() {
+    this.http.get<any>(this.getPostsURL).subscribe(res => {
+      this.posts = res;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+}
+
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 }
