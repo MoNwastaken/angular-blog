aa7d360a39d02ef794147ffd3d64fc23e674c70c
MoNwastaken <nhcmon@gmail.com>
2020-08-30 14:06:14 +0200

    Posts load from the server

--- src/app/home/posts/posts.component.ts
+++ src/app/home/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from "../../services/api.service";
 
 @Component({
   selector: 'app-posts',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
   }
